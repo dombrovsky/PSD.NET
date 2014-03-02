86309a7a62d99abeec5d0bceca4999dd28653363
Volodymyr Dombrovskyi <vladimir@goortom.com>
2014-03-02 23:21:52 +0200

    BigEndianBinaryReader added

---
+++ src/Psd.Net/BigEndianBinaryReader.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+
+namespace Psd.Net
+{
+    /// <summary>
+    /// Wraps a stream and provides convenient read functionality for strings and primitive types in big-endian format. 
+    /// </summary>
+    public sealed class BigEndianBinaryReader : BinaryReader
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BigEndianBinaryReader"/> class.
+        /// </summary>
+        /// <param name="stream">The stream.</param>
+        public BigEndianBinaryReader(Stream stream)
+            : base(stream)
+        {
+        }
+
+        /// <summary>
+        /// Reads a 2-byte signed integer from the current stream and advances the current position of the stream by two bytes.
+        /// </summary>
+        /// <returns>
+        /// A 2-byte signed integer read from the current stream.
+        /// </returns>
+        public override short ReadInt16()
+        {
+            return BitConverter.ToInt16(ReadReverse(2), 0);
+        }
+
+        /// <summary>
+        /// Reads a 4-byte signed integer from the current stream and advances the current position of the stream by four bytes.
+        /// </summary>
+        /// <returns>
+        /// A 4-byte signed integer read from the current stream.
+        /// </returns>
+        public override int ReadInt32()
+        {
+            return BitConverter.ToInt32(ReadReverse(4), 0);
+        }
+
+        /// <summary>
+        /// Reads an 8-byte signed integer from the current stream and advances the current position of the stream by eight bytes.
+        /// </summary>
+        /// <returns>
+        /// An 8-byte signed integer read from the current stream.
+        /// </returns>
+        public override long ReadInt64()
+        {
+            return BitConverter.ToInt64(ReadReverse(8), 0);
+        }
+
+        /// <summary>
+        /// Reads a 2-byte unsigned integer from the current stream using little-endian encoding and advances the position of the stream by two bytes.
+        /// </summary>
+        /// <returns>
+        /// A 2-byte unsigned integer read from this stream.
+        /// </returns>
+        public override ushort ReadUInt16()
+        {
+            return BitConverter.ToUInt16(ReadReverse(2), 0);
+        }
+
+        /// <summary>
+        /// Reads a 4-byte unsigned integer from the current stream and advances the position of the stream by four bytes.
+        /// </summary>
+        /// <returns>
+        /// A 4-byte unsigned integer read from this stream.
+        /// </returns>
+        public override uint ReadUInt32()
+        {
+            return BitConverter.ToUInt32(ReadReverse(4), 0);
+        }
+
+        /// <summary>
+        /// Reads an 8-byte unsigned integer from the current stream and advances the position of the stream by eight bytes.
+        /// </summary>
+        /// <returns>
+        /// An 8-byte unsigned integer read from this stream.
+        /// </returns>
+        public override ulong ReadUInt64()
+        {
+            return BitConverter.ToUInt64(ReadReverse(8), 0);
+        }
+
+        private byte[] ReadReverse(int count)
+        {
+            var bufer = ReadBytes(count);
+            Array.Reverse(bufer);
+            return bufer;
+        }
+    }
+}
